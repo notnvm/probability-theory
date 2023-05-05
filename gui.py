4143e514e4bdd162b3434c1dca7de155913538be
notnvm <auraz616@mail.ru>
2023-05-05 19:15:37 +0300

    some fixes in menu

--- gui.py
+++ gui.py
@@ -13,7 +13,7 @@ class Gui:
         master["bg"] = "#252526" 
         self.master = master
         
-        self.label_p = tk.Label(master, text='Введите значение p:', foreground="white",background='#252526', font='Arial 12')
+        self.label_p = tk.Label(master, text='Введите кол-во экcп.:', foreground="white",background='#252526', font='Arial 12')
         self.label_p.place(x=0, y=0)
         self.label_n = tk.Label(master, text='Введите значение N:', foreground="white",background='#252526', font='Arial 12')
         self.label_n.place(x=0, y=40)
@@ -48,29 +48,28 @@ class Gui:
         self.ax.tick_params(axis='y', colors='white')
         chart = FigureCanvasTkAgg(self.fig, master)
         chart.get_tk_widget().place(x=680, y=350)
-        
-    def solve_callback(self):
+   
+    def solve_callback(self):   
         self.ax.clear()
         self.ax.grid(True)
         self.ax.set_title("График функции распределения", color='white')
         
-        
-        p = float(self.p_val.get())
+        p = int(self.p_val.get())
         n = int(self.n_val.get())
         m = int(self.m_val.get())
         r = int(self.r_val.get())
+        
         solution = task.Solution(p, n, m, r)
         first_task = solution.first()
-        print(first_task)
-        math_expect, average, diff_avg, dispersion, s2,  diff_disp, median, R = solution.second()
+        ch = solution.second()
 
         heads = ["y_i", "n_i", "n_i / n"]
         self.table_ft = ttk.Treeview(self.master, show='headings', columns=heads, height=4)
         for header in heads:
             self.table_ft.heading(header, text=header, anchor='center')
             self.table_ft.column(header, anchor='center', width=455)
-        for i in range(r+1):
-            self.table_ft.insert("", tk.END, values=(i, first_task[i], first_task[i]/n))
+        for yj, nj in first_task:
+            self.table_ft.insert("", tk.END, values=(yj, nj, nj/p))
         self.scrollbary = tk.Scrollbar(self.master, orient=tk.VERTICAL)
         self.table_ft.configure(yscrollcommand=self.scrollbary.set)
         self.scrollbary.configure(command=self.table_ft.yview)
@@ -80,53 +79,41 @@ class Gui:
         for topic in topics:
             self.table_sd.heading(topic, text=topic, anchor='center')
             self.table_sd.column(topic, anchor='center', width=175)
-        self.table_sd.insert("", tk.END, values=(math_expect, average, diff_avg, dispersion, s2,  diff_disp, median, R))
+        self.table_sd.insert("", tk.END, values=(ch[0], ch[1], ch[2], ch[3], ch[4],  ch[5], ch[6], ch[7]))
         
         parts = ['y_j', 'P(n = y_j)', 'n_j / n']
+        probability = solution.probability()
         self.probs_table = ttk.Treeview(self.master, show='headings', columns=parts, height=4)
         for part in parts:
             self.probs_table.heading(part, text=part, anchor='center')
             self.probs_table.column(part, anchor='center', width=455)
-        max_deviation = 0
-        # prob_res = []
-        for i in range(r+1):
-            probability = (math.comb(m, (i))*math.comb(n-m, r-(i)))/math.comb(n,r)
-            # prob_res.append(probability)
-            if math.fabs(first_task[i]/n - probability) > max_deviation:
-                max_deviation = first_task[i]/n - probability
-            self.probs_table.insert("", tk.END, values=((i), probability, first_task[i]/n))
-        
-        self.label_max_deviation = tk.Label(self.master, text=f'Величина max|n_j / n - P(n = y_j)| = {round(max_deviation, 4)}', foreground="white",background='#252526', font='Arial 12')
-        self.label_max_deviation.place(x=0, y=180)  
-         
-        # print(sum(prob_res))        
-        self.scrollbar = ttk.Scrollbar(self.master, orient=tk.VERTICAL)
-        self.table_ft.configure(yscrollcommand=self.scrollbar.set)
-        self.scrollbar.configure(command=self.probs_table.yview)
+        for xi, ni in first_task:
+            self.probs_table.insert("", tk.END, values=(xi, probability[first_task.index((xi,ni))], ni/m))
+            
         self.table_ft.place(x=320, y=0)
         self.table_sd.place(x=320, y=110)
         self.probs_table.place(x=320, y=180)
         
-        solution.generate_plot_data()
+        tdf = solution.theoretical_distribution_function()
+        sdf = solution.sample_distribution_function()
+        interval_values = [i for i,_ in first_task]
         
-        self.ax.plot((-2, 1), (0, 0), color='red', linewidth=2)
-        self.ax.plot((-2, 1), (0, 0), color='blue', linewidth=2)
-        for i in range(r-1):
-            self.ax.plot((i, i+1), (solution.sums[i], solution.sums[i]),
-                         color='red', linewidth=2)
-            self.ax.plot((i, i+1), (solution.prefix_sums[i], solution.prefix_sums[i]),
-                         color='blue', linewidth=2)
-            
-        self.ax.plot((r-1, r), (1, 1), color='red', linewidth=2, label='F(x)')
-        self.ax.plot((r-1, r), (1, 1), color='blue', linewidth=2, label='F^(x)')
+        for i in range(0, len(interval_values) - 1):
+            plt.hlines(sdf[i + 1], interval_values[i], interval_values[i + 1], color='red')        
+        plt.hlines(0, -2, interval_values[0], color='red')
+        plt.hlines(1, interval_values[-1], interval_values[-1]+2, color='red', label='F^(x)')
+        
+        for i in range(0, len(interval_values) - 1):
+            plt.hlines(tdf[i + 1], interval_values[i], interval_values[i + 1], color='blue')           
+        plt.hlines(0, -2, interval_values[0], color='red')
+        plt.hlines(1, interval_values[-1], interval_values[-1]+2, color='blue', label='F(x)')
+        
+        plt.title(r'D=max|$\hat{F}$η(x) - Fη(x)|' + f' = {round(solution.measure_of_discrepancy(), 5)}')
 
         self.ax.grid(True)
         self.ax.legend()
         chart = FigureCanvasTkAgg(self.fig, self.master)
         chart.get_tk_widget().place(x=680, y=350)
-        
-        self.label_max_diff_f = tk.Label(self.master, text=f'Мера расхождения D = {round(solution.diff, 4)}', foreground="white",background='#252526', font='Arial 12')
-        self.label_max_diff_f.place(x=0, y=200)  
         
         
 window = tk.Tk()
