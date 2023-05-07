e59823ee4842955dff4aec2419404ec9cbdf76f2
notnvm <auraz616@mail.ru>
2023-05-07 13:36:33 +0300

    fix for tdf and measure of discrepancy

--- pt.py
+++ pt.py
@@ -5,27 +5,26 @@ from collections import Counter
 
 class Solution:
     def __init__(self, p: int, n: int, m: int, r: int):
-        """ 
+        """
             В лотерее среди N билетов M выигрышных. Игрок покупает r билетов.
             С.в. η — число выигрышных билетов среди купленных.
-            
+
             p: int - Число экспериментов
             n: int - Общее число билетов
             m: int - Число выигрышных билетов
             r: int - Число купленных билетов
         """
-        
+
         self.p = p
         self.n = n
         self.m = m
         self.r = r
-        self.win = [1]*(r+1)
+        self.win = [1] * (r + 1)
         self.cnt = {}
-        
+
     def first(self):
         """Simulation of an experiment\n
             Hypergeometric distribution"""
-            
         ar = []
         for j in range(self.p):
             count = 0
@@ -33,7 +32,7 @@ class Solution:
             mm = self.m
             for i in range(self.r):
                 u = random.random()
-                if u < mm/nn:
+                if u < mm / nn:
                     count += 1
                     nn = nn - 1
                     mm = mm - 1
@@ -43,90 +42,71 @@ class Solution:
 
         self.cnt = Counter(ar)
         self.series = sorted(self.cnt.most_common())
-        print(self.series)
-        # for yj, nj in sorted(self.cnt.most_common()):
-        #     print(f'\nyj = {yj}, nj = {nj}, nj/n = {nj/self.n}\n')
-        # print(f'\n\nself.m = {self.m}, self.n = {self.n}')
-        # self.series = sorted(self.cnt.most_common())
-        # print(self.series)
-        return self.series #! sorted(self.cnt.most_common())
-          
+
+        return self.series  
+
     def average(self):
         """compute sample mean"""
-        
         avg = 0
         for xi, ni in self.series:
             avg += xi * ni
         avg /= self.p
+        
         return avg
-    
+
     def mean(self):
         """compute math expectation"""
-        
-        return (self.r * self.m)/self.n
-    
+
+        return (self.r * self.m) / self.n
+
     def variance(self):
         """compute dispersion"""
-        
-        return ((self.r*self.m)/(self.n-1))*(1-self.m/self.n)*(1-self.r/self.n)   
-    
+
+        return ((self.r * self.m) / (self.n - 1)) * (1 - self.m / self.n) * (1 - self.r / self.n)
+
     def sample_variance(self):
         """compute sample dispersion"""
-        
         sv = 0
         avg = self.average()
         for xi, ni in self.series:
-            sv += ((xi - avg)**2)*ni
+            sv += ((xi - avg) ** 2) * ni
         sv /= self.p
+        
         return sv
-         
+
     def sample_size(self):
         return self.series[-1][0] - self.series[0][0]
-    
+
     def sample_median(self):
         x = [i for i, _ in self.series]
         return (x[len(x) // 2] + x[len(x) // 2 - 1]) / 2 if len(x) % 2 == 0 else x[len(x) // 2]
-    
+
     def probability(self):
         """
             compute theoretical probability\n
             C(n, k)\n
             P(ξ = xi) = C(m, xi) * C(n-m, r-xi) / C(n, r)
         """
-        
         probability = []
-        for xi,_ in self.series:
-            probability.append((math.comb(self.m, xi)*math.comb(self.n-self.m, self.r-xi))/math.comb(self.n,self.r))
-        # print(probability)
-        # print(sum(probability))
+        for i in range(self.r+1):
+            probability.append(
+                (math.comb(self.m, i) * math.comb(self.n - self.m, self.r - i)) / math.comb(self.n, self.r))
+
         return probability
-    
-    def max_deviation(self):
-        """
-             compute max deviation between relative frequency and probability\n
-             max|nj/n - P(ξ = xi)|
-        """
-        probability = self.probability()
-        relative_frequency = [i/self.p for _, i in self.series]
-        max_deviation = -100
-        for i in range(len(probability)):
-            if max_deviation < abs(relative_frequency[i] - probability[i]):
-                max_deviation = abs(relative_frequency[i] - probability[i])
-        return max_deviation
-        
+
     def second(self):
         """ compute all numerical characteristics\n
-            return [E, sample_mean_x, abs_diff_mean, D, SS, abs_diff_variance, self.sample_median(), self.sample_size()]  
+            return [E, sample_mean_x, abs_diff_mean, D, SS, abs_diff_variance, self.sample_median(), self.sample_size()]
         """
-        
         E = self.mean()
         sample_mean_x = self.average()
         abs_diff_mean = abs(E - sample_mean_x)
         D = self.variance()
         SS = self.sample_variance()
         abs_diff_variance = abs(D - SS)
-        return [E, sample_mean_x, abs_diff_mean, D, SS, abs_diff_variance, self.sample_median(), self.sample_size()] 
-    
+        
+        return [E, sample_mean_x, abs_diff_mean, D, SS, abs_diff_variance, self.sample_median(), self.sample_size()]
+
     def theoretical_distribution_function(self):
         """
              compute theoretical distribution function\n
@@ -136,12 +116,22 @@ class Solution:
         tdf = [0]
         for i in range(len(probability)):
             tdf.append(tdf[i] + probability[i])
-        
-        # print(f'prob = {probability}')
-        # print(f'tdf = {tdf}')
+
         return tdf
 
+    def get_tdf_intervals(self):
+        """
+             compute theoretical distribution function intervals\n
+             exmpl: (x_min, x_max, probability)
+        """
+        tdf = self.theoretical_distribution_function()
+        tdf_intervals = [(-2, 0, 0)]
+        for i in range(self.r):
+            tdf_intervals.append((i, i+1, tdf[i+1]))
+        tdf_intervals.append((self.r, self.r+2, 1))
 
+        return tdf_intervals
+          
     def sample_distribution_function(self):
         """
              compute sample distribution function\n
@@ -149,31 +139,35 @@ class Solution:
         """
         sdf = [0]
         for _, ni in self.series:
-            sdf.append(sdf[self.series.index((_, ni))] + ni/self.p)
-            
-        # print(f'rel_freq = {[i for _,i in self.series]}')
-        # print(f'sdf = {sdf}')
+            sdf.append(sdf[self.series.index((_, ni))] + ni / self.p)
+
         return sdf
+    
+    def get_sdf_intervals(self):
+        """
+             compute sample distribution function intervals\n
+             exmpl: (x_min, x_max, probability)
+        """   
+        sdf = self.sample_distribution_function()
+        sdf_intervals = [(-2, self.series[0][0], 0)]
+        for i in range(len(self.series) - 1):
+            sdf_intervals.append((self.series[i][0], self.series[i+1][0], sdf[i+1]))
+        sdf_intervals.append((self.series[-1][0], self.r+2, 1))
+
+        return sdf_intervals
 
     def measure_of_discrepancy(self):
-        sdf = self.sample_distribution_function()
-        tdf = self.theoretical_distribution_function()
-        discrepancy = max([abs(sdf[i] - tdf[i]) for i in range(len(tdf))])
+        """
+             compute discrepancy of sdf and tdf\n
+             D = max|nj/n - P(ξ = xj)|
+        """
+        discrepancy = 0
+        sdfi = self.get_sdf_intervals()
+        tdfi = self.get_tdf_intervals()
+        
+        for xmin, xmax, probability in sdfi:
+            for i in range(len(tdfi)):
+                if (xmin == tdfi[i][0] or xmax == tdfi[i][1]) and abs(probability - tdfi[i][2]) > discrepancy:
+                    discrepancy = abs(probability - tdfi[i][2])
+
         return discrepancy
-
-
-if __name__ == '__main__':
-    p=int(input('p='))
-    n=int(input('n='))    
-    m=int(input('m='))
-    r=int(input('r='))
-    s = Solution(p, n, m, r)
-    s.first()
-    # print(f's.first() = {s.first()}')
-    # print(f's.first()[0][0] = {s.first()[0][0]}')
-    # tdf = s.theoretical_distribution_function()
-    # sdf = s.sample_distribution_function()
-    # print(f'series.len = {len(s.series)}')
-    # print(f'tdf.len = {len(tdf)}')
-    # print(f'sdf.len = {len(sdf)}')
-    # print('\n', sum(s.probability()))
